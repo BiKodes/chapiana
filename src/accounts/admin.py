16d3732d0f11548775ab47212a9fd6d0010d7c70
Biko Steven <bikocodes@gmail.com>
2025-02-25 22:56:50 +0300

    feat: accounts login viewset

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from src.accounts.forms import CustomUserRegisterForm, CustomUserChangeForm
+from src.accounts.models import ChapianaUser, OneTimePassword
+from django.db import models
+from django import forms
 
-# Register your models here.
+
+class CustomChapianaUserAdmin(UserAdmin):
+    add_form = CustomUserRegisterForm
+    form = CustomUserChangeForm
+    model = ChapianaUser
+    list_display = ("username", "first_name", "last_name", "email", "is_staff", "is_active", "is_superuser")
+    list_filter = ("is_staff", "is_active", "is_superuser", "date_joined", "last_login")
+    readonly_fields = ("date_joined", "last_login")
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("-date_joined")
+    formfield_overrides = {
+        models.PositiveIntegerField: {"widget": forms.NumberInput(attrs={"size": "15"})},
+    }
+
+    fieldsets = (
+        (None, {"fields": ("password",)}),
+        ("personal info", {"fields": (
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "last_login",
+            "date_joined",
+        )}),
+        ("permissions", {"fields": ("is_superuser", "is_staff", "is_active", "groups", "user_permissions")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "username",
+                "email",
+                "first_name",
+                "last_name",
+                "password1", "password2", "is_superuser", "is_staff",
+                "is_active", "groups", "user_permissions",
+            )
+        })
+    )
+
+admin.site.register(ChapianaUser, CustomChapianaUserAdmin)
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views.decorators.csrf import csrf_protect
+from django.utils.decorators import method_decorator
 
-# Create your views here.
+class LoginViewSet(viewsets.ViewSet):
+    """
+    A ViewSet for user login.
+
+    GET
+
+      - If the user is authenticated, redirects to the chat lobby.
+      - Otherwise, renders the login (authentication) template.
+
+    POST
+
+      - Authenticates the user based on provided username and password.
+      - On success, logs the user in and returns a success JSON response.
+      - On failure, returns an appropriate error response.
+    """
+
+    @method_decorator(csrf_protect)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def list(self, request):
+        """
+        Handles GET requests.
+
+        If the user is authenticated, redirects to 'chat:lobby'.
+        Otherwise, renders the 'account_app/auth.html' template.
+        """
+        if request.user.is_authenticated:
+            return redirect('chat:lobby')
+        return render(request, 'account_app/auth.html')
+
+    def create(self, request):
+        """
+        Handles POST requests to authenticate and log in a user.
+
+        Expects 'username' and 'password' in the request data.
+
+        Returns:
+            - 200 OK with username on successful login.
+            - 401 Unauthorized if authentication fails.
+            - 400 Bad Request if username or password is missing.
+        """
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return Response(
+                    {'status': 200, 'username': user.username},
+                    status=status.HTTP_200_OK
+                )
+            else:
+                return Response({'status': 401}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response({'status': 400}, status=status.HTTP_400_BAD_REQUEST)
